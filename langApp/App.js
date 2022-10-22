5a26276ab0f9e4b817c136c7fbb46998188e2c48
Soobin Kim <soobin.kim@fruitsfamily.com>
2022-10-22 10:41:40 +0900

    [1022] drag and drop project

--- langApp/App.js
+++ langApp/App.js
@@ -1,12 +1,157 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Animated, Easing } from "react-native";
+import React, { useRef, useState } from "react";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
+import { PanResponder } from "react-native";
+
+const BLACK_COLOR = "#1e272e";
+const GREY = "#485460";
+const GREEN = "#2ecc71";
+const RED = "#e74c3c";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
+`;
+const Edge = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 100px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${GREY};
+  border-radius: 50px;
+`;
+const Word = styled.Text`
+  font-size: 38px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+const Center = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+`;
+const IconCard = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+  z-index: 10;
+  position: absolute;
+`;
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+  // Values
+  const opacity = useRef(new Animated.Value(1)).current;
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const scaleOne = position.y.interpolate({
+    inputRange: [-300, -80],
+    outputRange: [1.8, 1],
+    extrapolate: "clamp",
+  });
+  const scaleTwo = position.y.interpolate({
+    inputRange: [80, 300],
+    outputRange: [1, 1.8],
+    extrapolate: "clamp",
+  });
 
-export default App
+  // Animations
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  });
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const goHome = Animated.spring(position, {
+    toValue: { x: 0, y: 0 },
+    useNativeDriver: true,
+    bounciness: 10,
+  });
+  const onDropScale = Animated.timing(scale, {
+    toValue: 0,
+    easing: Easing.linear,
+    duration: 100,
+    useNativeDriver: true,
+  });
+  const onDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    easing: Easing.linear,
+    duration: 100,
+    useNativeDriver: true,
+  });
+
+  // Pan Responders
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderRelease: (_, { dy }) => {
+        if (dy < -250 || dy > 250) {
+          Animated.sequence([
+            Animated.parallel([onDropScale, onDropOpacity]),
+            Animated.timing(position, {
+              toValue: 0,
+              useNativeDriver: true,
+              duration: 100,
+              easing: Easing.linear,
+            }),
+          ]).start(nextIcon);
+        } else {
+          Animated.parallel([onPressOut, goHome]).start();
+        }
+      },
+    })
+  ).current;
+
+  // State
+  const [index, setIndex] = useState(0);
+  const nextIcon = () => {
+    setIndex((prev) => prev + 1);
+    Animated.parallel([
+      Animated.spring(scale, { toValue: 1, useNativeDriver: true }),
+    Animated.spring(opacity, { toValue: 1, useNativeDriver: true })
+    ]).start()
+  };
+
+  return (
+    <Container>
+      <Edge>
+        <WordContainer style={{ transform: [{ scale: scaleOne }] }}>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
+      </Edge>
+      <Center>
+        <IconCard
+          {...panResponder.panHandlers}
+          style={{
+            opacity: opacity,
+            transform: [...position.getTranslateTransform(), { scale }],
+          }}
+        >
+          <Ionicons name={icons[index]} color={GREY} size={76} />
+        </IconCard>
+      </Center>
+      <Edge>
+        <WordContainer style={{ transform: [{ scale: scaleTwo }] }}>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
+      </Edge>
+    </Container>
+  );
+};
+
+export default App;
