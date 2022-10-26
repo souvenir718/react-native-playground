a38591819477fc8467fcd96df47215f87b4b36b9
Soobin Kim <soobin.kim@fruitsfamily.com>
2022-10-26 09:56:52 +0900

    [1026] initial setting about diary app

---
+++ diaryApp/App.js
@@ -0,0 +1,13 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
